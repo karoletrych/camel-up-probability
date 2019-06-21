23f86d807d6e2665239173e9f274302ee43efe7b
Karol Etrych <karol.etrych@gmail.com>
2019-06-21 20:33:29 +0200

    refactor rollsequences

--- src/Home/RollSequences.fs
+++ src/Home/RollSequences.fs
@@ -4,17 +4,17 @@ open Home.Types
 let allCamelsSet = [Yellow; Blue; Orange; Green; White] |> Set.ofList
 
 
-let allRollCombinations dicesLeft =
+let allRollCombinations (dicesLeft : Camel list) : DiceRoll list seq =
     let rec rollCombinations
-        (acc : DiceRoll list)
-        (possibleCamels: Camel List)
-        : DiceRoll list seq =
+        acc
+        possibleCamels
+         =
+        let possibleRollsNow =
+            [for i in [1;2;3] do
+                for camel in possibleCamels do
+                    yield {Camel = camel; Count = i}
+            ]
         seq {
-            let possibleRollsNow =
-                [for i in [1;2;3] do
-                    for camel in possibleCamels do
-                        yield {Camel = camel; Count = i}
-                ]
             match possibleRollsNow with
             | [] -> yield acc
             | rolls ->
